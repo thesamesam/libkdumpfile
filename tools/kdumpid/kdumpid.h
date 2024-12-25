c3b336774dba806c2b6403113fd6ac419d9959b0
Petr Tesarik <petr@tesarici.cz>
2024-12-25 21:10:05 +0100

    kdumpid: Get rid of endian.h
    This include file may conflict with the system <endian.h> (courtesy of
    Automake adding -I. to compiler flags).
    
    Move the content of endian.h to kdumpid.h, because it is included only from
    that one place anyway.
    
    Signed-off-by: Petr Tesarik <petr@tesarici.cz>

--- tools/kdumpid/kdumpid.h
+++ tools/kdumpid/kdumpid.h
@@ -5,9 +5,47 @@
 
 #include <stdint.h>
 #include <unistd.h>
+#include <endian.h>
 #include <libkdumpfile/kdumpfile.h>
 
-#include "endian.h"
+/* Older glibc didn't have the byteorder macros */
+#ifndef be16toh
+
+#include <byteswap.h>
+
+# if __BYTE_ORDER == __LITTLE_ENDIAN
+#  define htobe16(x) bswap_16(x)
+#  define htole16(x) (x)
+#  define be16toh(x) bswap_16(x)
+#  define le16toh(x) (x)
+
+#  define htobe32(x) bswap_32(x)
+#  define htole32(x) (x)
+#  define be32toh(x) bswap_32(x)
+#  define le32toh(x) (x)
+
+#  define htobe64(x) bswap_64(x)
+#  define htole64(x) (x)
+#  define be64toh(x) bswap_64(x)
+#  define le64toh(x) (x)
+# else
+#  define htobe16(x) (x)
+#  define htole16(x) bswap_16(x)
+#  define be16toh(x) (x)
+#  define le16toh(x) bswap_16(x)
+
+#  define htobe32(x) (x)
+#  define htole32(x) bswap_32(x)
+#  define be32toh(x) (x)
+#  define le32toh(x) bswap_32(x)
+
+#  define htobe64(x) (x)
+#  define htole64(x) bswap_64(x)
+#  define be64toh(x) (x)
+#  define le64toh(x) bswap_64(x)
+# endif
+
+#endif
 
 #define INVALID_ADDR	((uint64_t)-1ULL)
 
